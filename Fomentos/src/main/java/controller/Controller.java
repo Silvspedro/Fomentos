cb95bbd938602a31e8721fab3a582017c6e35750
Silvspedro <pedro.asilva03@gmail.com>
2023-10-26 17:12:52 -0300

    HTML E CSS prontos e Login e cadastro funcionando em back

---
+++ Fomentos/src/main/java/controller/Controller.java
@@ -0,0 +1,91 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.DAO;
+import model.JavaBeans;
+
+/**
+ * Servlet implementation class ValidacaoLogin
+ */
+@WebServlet(urlPatterns = { "/Logar", "/insert" })
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	DAO dao = new DAO();
+	JavaBeans usuario = new JavaBeans();
+
+	public Controller() {
+		super();
+	}
+	//Metodo de decisão e direcionamento para prosseguimento seguinte
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String action = request.getServletPath(); // Armazena o caminho da requisição.
+		System.out.println(action);
+		if (action.equals("/Logar")) {
+			System.out.println("Entrou");
+			validacaoLogin(request, response);
+		} else if (action.equals("/insert")) {
+			novoUsuario(request, response);
+		}
+	}
+	//Metodo para adicionar novo Usuario
+	protected void novoUsuario(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Teste de recebimento dos dados do formulário.
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("email"));
+		System.out.println(request.getParameter("senha"));
+		System.out.println(request.getParameter("cpf"));
+		// Setar as variáveis JavaBeans.
+		usuario.setNome_usuario(request.getParameter("nome"));
+		usuario.setEmail_usuario(request.getParameter("email"));
+		usuario.setSenha_usuario(request.getParameter("senha"));
+		usuario.setCpf_usuario(request.getParameter("cpf"));
+		// Invocar o método inserirUsuario passando o objeto contato.
+		dao.inserirUsuario(usuario);
+		// Redirecionar para o documento login.html
+		response.sendRedirect("tela-de-login.html");
+	}
+	
+	//Metodo para validar login do Usuario
+		protected void validacaoLogin(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+
+			// Varioaveis utilizadas para validar login
+			String emailDigitado = request.getParameter("email");
+			String senhaDigitada = request.getParameter("senha");
+			String emailValidado = null;
+			String senhaValidada = null;
+			// Criando um objeto para receber os dados javaBeans
+			ArrayList<JavaBeans> lista = dao.validacaoLogin();
+			// Retirada de itens da lista para comparação
+			for (int i = 0; i < lista.size(); i++) {
+				// Teste de recebimento de dados
+						System.out.println(lista.get(i).getId_usuario());
+						System.out.println(lista.get(i).getEmail_usuario());
+						System.out.println(lista.get(i).getSenha_usuario());
+				if (emailDigitado.equals(lista.get(i).getEmail_usuario())
+						&& senhaDigitada.equals(lista.get(i).getSenha_usuario())) {
+					emailValidado = lista.get(i).getEmail_usuario();
+					senhaValidada = lista.get(i).getSenha_usuario();
+				}
+
+			}
+			// Redirecionamento para pagina devida
+			if (emailDigitado.equals(emailValidado) && senhaDigitada.equals(senhaValidada)) {
+				response.sendRedirect("pagina_pos_login.html");
+			} else {
+				response.sendRedirect("termos-uso.html");
+			}
+		}
+		
+}
---
+++ Fomentos/src/main/java/model/JavaBeans.java
@@ -0,0 +1,98 @@
+package model;
+
+public class JavaBeans {
+    private String id_usuario;
+    private String email_usuario;
+    private String senha_usuario;
+    private String confirSenha;
+    private String nome_usuario;
+    private String cpf_usuario;
+    
+
+    public JavaBeans() {
+        super();
+    }
+
+    public JavaBeans(String ID_USUARIO, String NOME_USUARIO, String EMAIL_USUARIO, String SENHA_USUARIO,String CPF_USUARIO) {
+        super();
+        this.id_usuario = ID_USUARIO;
+        this.nome_usuario = NOME_USUARIO;
+        this.email_usuario = EMAIL_USUARIO;
+        this.senha_usuario = SENHA_USUARIO;
+        this.cpf_usuario = CPF_USUARIO;
+    }
+
+    public String getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(String id_usuario) {
+        if (id_usuario != null && !id_usuario.isEmpty()) {
+            this.id_usuario = id_usuario;
+        }
+    }
+
+    public String getEmail_usuario() {
+        return email_usuario;
+    }
+
+    public void setEmail_usuario(String email_usuario) {
+        if (email_usuario != null && !email_usuario.isEmpty()) {
+            this.email_usuario = email_usuario;
+        } else {
+            throw new IllegalArgumentException("O campo email_usuario é obrigatório");
+        }
+    }
+
+    public String getSenha_usuario() {
+        return senha_usuario;
+    }
+
+    public void setSenha_usuario(String senha_usuario) {
+        if (senha_usuario != null && !senha_usuario.isEmpty()) {
+            this.senha_usuario = senha_usuario;
+        } else {
+            throw new IllegalArgumentException("O campo senha_usuario é obrigatório");
+        }
+    }
+    
+    
+
+    public String getConfirSenha() {
+		return confirSenha;
+	}
+
+    public void setConfirSenha(String confirSenha) {
+        if (confirSenha != null && !confirSenha.isEmpty()) {
+            this.confirSenha = confirSenha;
+        } else {
+            throw new IllegalArgumentException("O campo senha_usuario é obrigatório");
+        }
+    }
+
+	public String getNome_usuario() {
+        return nome_usuario;
+    }
+
+    public void setNome_usuario(String nome_usuario) {
+        if (nome_usuario != null && !nome_usuario.isEmpty()) {
+            this.nome_usuario = nome_usuario;
+        } else {
+            throw new IllegalArgumentException("O campo nome_usuario é obrigatório");
+        }
+    }
+
+	public String getCpf_usuario() {
+		return cpf_usuario;
+	}
+
+	public void setCpf_usuario(String cpf_usuario) {
+		this.cpf_usuario = cpf_usuario;
+	}
+    
+    
+}
+
+	
+
+	
